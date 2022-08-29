522a23a568fd563bce641370fe2eedfe3a8b2381
roshanpalkandwar <roshanpalkandwar@gmail.com>
2022-08-29 10:15:41 +0530

    [roshan]flip coin program

---
+++ basicCoreJavaProgram/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace basicCoreJavaProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welecome to basic java core program");
+            Console.WriteLine("chooes option\n1. flip coin");
+            int opt=Convert.ToInt32(Console.ReadLine());
+
+            switch(opt)
+            {
+                case 1:
+                    flipCoin.calculatePercentage();
+                    break;
+            }
+        }
+    }
+}
---
+++ basicCoreJavaProgram/flipCoin.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace basicCoreJavaProgram
+{
+    internal class flipCoin
+    {
+        public static void calculatePercentage()
+        {
+            int headCount=0,tailCount=0;
+            double headPercentage;
+            double tailPercentage;
+
+            Console.WriteLine("please enter the value of flip");
+            int flipcoin=Convert.ToInt32(Console.ReadLine());
+
+            if(flipcoin>0)
+            {
+                for(int i=0;i<flipcoin;i++)
+                {
+                    Random random = new Random();
+                    double number=random.NextDouble();
+                    if(number>0.5)
+                    {
+                        Console.WriteLine("head");
+                        headCount++;
+                    }
+                    else
+                    {
+                        Console.WriteLine("tail");
+                        tailCount++;
+                    }
+
+                }
+                headPercentage=headCount*100/flipcoin;
+                tailPercentage=tailCount*100/flipcoin;
+                Console.WriteLine("head percentage  " + headPercentage + "  " + "tail parcentage  " + tailPercentage);
+            }
+            else
+            {
+                Console.WriteLine("enter the positive number");
+            }
+        }
+    }
+}
