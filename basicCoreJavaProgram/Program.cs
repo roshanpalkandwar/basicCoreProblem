38ff266b9269d40f39c3ca29c820361e7f4ebdf7
roshanpalkandwar <roshanpalkandwar@gmail.com>
2022-08-30 10:05:10 +0530

    [roshan]leap year or not

--- basicCoreJavaProgram/Program.cs
+++ basicCoreJavaProgram/Program.cs
@@ -5,13 +5,16 @@
         static void Main(string[] args)
         {
             Console.WriteLine("welecome to basic java core program");
-            Console.WriteLine("chooes option\n1. flip coin");
+            Console.WriteLine("chooes option\n1. flip coin\n2.leap year or not");
             int opt=Convert.ToInt32(Console.ReadLine());
 
             switch(opt)
             {
                 case 1:
                     flipCoin.calculatePercentage();
+                    break;
+                    case 2:
+                    leapYear.leapYear1();
                     break;
             }
         }
---
+++ basicCoreJavaProgram/leapYear.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace basicCoreJavaProgram
+{
+    internal class leapYear
+    {
+        public static void leapYear1()
+        {
+            Console.WriteLine("enter the year");
+            int n = Convert.ToInt32(Console.ReadLine());
+            if (n % 4 == 0)
+            {
+                Console.WriteLine("this is leap year");
+            }
+            else
+            {
+                Console.WriteLine("this is not leap year");
+            }
+    }   }
+
+}
